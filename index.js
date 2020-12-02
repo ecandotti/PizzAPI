50130182a4ad65a06ab94a39dff5781f33af1d54
ecandotti <enzo.candotti16@hotmail.com>
2020-12-02 21:53:49 +0100

    add routes for client

--- index.js
+++ index.js
@@ -4,20 +4,50 @@ const mongoose = require('mongoose')
 
 const app = express()
 const ResidenceRoutes = express.Router()
+const ClientRoutes = express.Router()
 
-const PORT = 3001
+const PORT = 8181
 let Residence = require('./residence.model')
+let Client = require('./client.model')
 
 app.use(bodyParser.json())
 mongoose.connect('mongodb://localhost/PizzApp', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const connection = mongoose.connection;connection.once('open', function() {
-    console.log("MongoDB database connection established successfully")
+    console.log("Connexion à la base de donnée MongoDB réussie")
 })
 
-ResidenceRoutes.route('/')
+/*          ROUTE RESIDENCE         */
+ResidenceRoutes.route('/list')
     .get(function(req, res) {
-        Residence.find(function(err, todos) {
+        Residence.find(function(err, list_res) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(list_res)
+            }
+        })
+    })
+
+ResidenceRoutes.route('/add')
+    .post(function(req, res) {
+        let residence = new Residence(req.body)
+        console.log(residence)
+        residence.save()
+            .then(residence => {
+                res.status(200).json({'residence': 'added successfully'})
+                console.log('saved')
+            })
+            .catch(err => {
+                res.status(400).json({'residence': err})
+                console.log('no saved')
+            })
+    })
+
+/*          ROUTE CLIENT         */
+ClientRoutes.route('/list')
+    .get(function(req, res) {
+        client.find(function(err, todos) {
             if (err) {
                 console.log(err)
             } else {
@@ -26,20 +56,24 @@ ResidenceRoutes.route('/')
         })
     })
 
-ResidenceRoutes.route('/addResidence')
+ClientRoutes.route('/add')
     .post(function(req, res) {
-        let residence = new Residence(req.body)
-        console.log(residence)
-        residence.save()
-            .then(residence => {
-                res.status(200).json({'residence': 'residence added successfully'})
+        let client = new Client(req.body)
+        console.log(req)
+        client.save()
+            .then(client => {
+                res.status(200).json({'client': 'added successfully'})
+                console.log('saved')
             })
             .catch(err => {
-                res.status(400).send('adding new residence failed')
+                res.status(400).json({'client': err})
+                console.log('no saved')
             })
     })
 
 app.use('/residences', ResidenceRoutes)
+app.use('/clients', ClientRoutes)
+
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT)
 })
