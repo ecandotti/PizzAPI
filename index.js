16267fa0df700850207599ee0b40c6c7733b6a00
ecandotti <enzo.candotti16@hotmail.com>
2020-12-05 22:41:51 +0100

    CORS Enable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('corse')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
